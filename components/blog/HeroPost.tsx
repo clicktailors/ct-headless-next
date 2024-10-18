ce953a8408f64a4f4e2a512e26bcd9e576d4c15b
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2024-10-17 19:28:22 -0500

    break up rendered jsx into sub-components

--- components/blog/HeroPost.tsx
+++ components/blog/HeroPost.tsx
@@ -35,38 +35,56 @@ export default function HeroPost({
 	author,
 	slug,
 }: HeroPostProps) {
+	const Heading = () => {
+		return (
+			<div>
+				<h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+					<Link
+						href={`/blog/posts/${slug}`}
+						className="hover:underline"
+						dangerouslySetInnerHTML={{ __html: title }}
+					/>
+				</h3>
+				<div className="mb-4 md:mb-0 text-sm text-gray-500">
+					<Date dateString={date} />
+				</div>
+			</div>
+		);
+	};
+
+	const Image = () => {
+		return (
+			<div className="mb-8 md:mb-16">
+				{coverImage && (
+					<CoverImage
+						title={title}
+						coverImage={coverImage}
+						slug={slug}
+					/>
+				)}
+			</div>
+		);
+	};
+
+	const TextArea = () => {
+		return (
+			<div>
+				<div
+					className="text-lg leading-relaxed mb-4"
+					dangerouslySetInnerHTML={{ __html: excerpt }}
+				/>
+				<Avatar author={author} />
+			</div>
+		);
+	};
+
 	return (
 		<Section>
 			<Container>
-				<div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-					<div>
-						<div className="mb-8 md:mb-16">
-							{coverImage && (
-								<CoverImage
-									title={title}
-									coverImage={coverImage}
-									slug={slug}
-								/>
-							)}
-						</div>
-					</div>
-					<div>
-						<h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-							<Link
-								href={`/blog/posts/${slug}`}
-								className="hover:underline"
-								dangerouslySetInnerHTML={{ __html: title }}
-							/>
-						</h3>
-						<div className="mb-4 md:mb-0 text-sm text-gray-500">
-							<Date dateString={date} />
-						</div>
-						<div
-							className="text-lg leading-relaxed mb-4"
-							dangerouslySetInnerHTML={{ __html: excerpt }}
-						/>
-						<Avatar author={author} />
-					</div>
+				<div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 my-20 md:my-28">
+					<Heading />
+					<Image />
+					<TextArea />
 				</div>
 			</Container>
 		</Section>
