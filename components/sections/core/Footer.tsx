88ab1667cbf1ff48e77851425fd1befd8a9966c1
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2024-10-30 22:11:37 -0500

    update footer coloring

--- components/sections/core/Footer.tsx
+++ components/sections/core/Footer.tsx
@@ -21,7 +21,7 @@ const Links: React.FC = () => {
 
 const Footer: React.FC = () => {
 	return (
-		<footer className="bg-accent-1 border-accent-2 footer footer-center p-16 bg-black/10 text-base-content rounded">
+		<footer className="border-accent-2 footer footer-center p-16 bg-black/10 text-base-content rounded">
 			<SiteTitle size="text-4xl" logoSize="size-8" />
 			<Links />
 			<Socials />
