47a5a040bf4f99660b64af13f352a6d68f30428c
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2024-12-15 22:58:47 -0600

    correct cmsConfig import

--- api/preview.ts
+++ api/preview.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createCMSProvider } from "../lib/cms/cms-factory";
-import { cmsConfig } from "../lib/config";
+import { getStaticSiteConfig } from "../lib/config";
 
 export default async function preview(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const { secret, id, slug } = req.query as { secret?: string; id?: string; slug?: string };
+	const { secret, id, slug } = req.query;
 
 	// Check the secret and next parameters
 	// This secret should only be known by this API route
@@ -17,29 +17,34 @@ export default async function preview(
 	) {
 		return res.status(401).json({ message: "Invalid token" });
 	}
-	// Fetch WordPress to check if the provided `id` or `slug` exists
-	const cms = createCMSProvider(cmsConfig.type);
-	const post = await cms.getPreviewPost(
-		id ? parseInt(id as string, 10) : (slug ? parseInt(slug as string, 10) : 0),
-		id ? "DATABASE_ID" : "SLUG"
-	);
 
-	// If the post doesn't exist prevent preview mode from being enabled
-	if (!post) {
-		return res.status(401).json({ message: "Post not found" });
+	// Fetch WordPress data
+	const config = getStaticSiteConfig();
+	const cms = createCMSProvider(config.cmsType);
+	
+	try {
+		// @ts-ignore
+		const post = await cms.getPreviewPost(id || slug, id ? "DATABASE_ID" : "SLUG");
+
+		if (!post) {
+			return res.status(401).json({ message: "Post not found" });
+		}
+
+		// Enable Preview Mode by setting the cookies
+		res.setPreviewData({
+			post: {
+				id: post.id,
+				slug: post.slug,
+				status: post.status,
+			},
+		});
+
+		// Redirect to the path from the fetched post
+		// We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
+		res.writeHead(307, { Location: `/blog/posts/${post.slug || slug}` });
+		res.end();
+	} catch (error) {
+		console.error("Preview Error:", error);
+		return res.status(500).json({ message: "Error reading preview post" });
 	}
-
-	// Enable Preview Mode by setting the cookies
-	res.setPreviewData({
-		post: {
-			id: post.databaseId,
-			slug: post.slug,
-			status: post.status,
-		},
-	});
-
-	// Redirect to the path from the fetched post
-	// We don't redirect to `req.query.slug` as that might lead to open redirect vulnerabilities
-	res.writeHead(307, { Location: `/posts/${post.slug || post.databaseId}` });
-	res.end();
 }
